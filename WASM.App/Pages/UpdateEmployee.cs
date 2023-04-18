89a69a082253c297bc0be381cc1c398d9327577c
Ahmed Khalaf Hassan Ali <AhmedKH@hits-egypt.local>
2023-04-18 13:43:16 +0200

    crud operations not completed

--- WASM.App/Pages/UpdateEmployee.cs
+++ WASM.App/Pages/UpdateEmployee.cs
@@ -4,24 +4,24 @@ using WASMLibrary.Models;
 
 namespace WASM.App.Pages
 {
-    public partial class EditEmployee
+    public partial class UpdateEmployee
     {
-        [Parameter]
-        public int EmpId { get; set; }
         [Inject]
         public IDataService<Employee> EmployeeDataService { get; set; }
         [Inject]
         public IDataService<Country> CountryDataService { get; set; }
+        [Parameter]
+        public int EmpId { get; set; }
         public IEnumerable<Country> Countries { get; set; }
         public Employee Employee { get; set; }
         public bool Saved = false;
         public string Status = string.Empty;
         public string Message = string.Empty;
-        public EditEmployee(IDataService<Employee> employeeDataService, IDataService<Country> countryDataService)
-        {
-            EmployeeDataService = employeeDataService;
-            CountryDataService = countryDataService;
-        }
+        //public UpdateEmployee(IDataService<Employee> employeeDataService, IDataService<Country> countryDataService)
+        //{
+        //    EmployeeDataService = employeeDataService;
+        //    CountryDataService = countryDataService;
+        //}
         protected async override Task OnInitializedAsync()
         {
             Countries = await CountryDataService.GetAllAsync();
